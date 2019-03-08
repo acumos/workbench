652730dcfb578b95dd098ea2c14f02438b03ee20
ak983d <ak983d@att.com>
2019-03-08 13:16:26 -0600

    Initial code check in for ML Workbench UI Project.
    Change-Id: I3e767355b7ed565f9bbacfdf2e63bea94ea21e28
    Signed-off-by: ak983d <ak983d@att.com>

---
+++ home-webcomponent/src/main/resources/src/app/@theme/components/header/header.component.ts
@@ -0,0 +1,57 @@
+/*
+===============LICENSE_START=======================================================
+Acumos Apache-2.0
+===================================================================================
+Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+===================================================================================
+This Acumos software file is distributed by AT&T and Tech Mahindra
+under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+===============LICENSE_END=========================================================
+*/
+
+import { Component, Input, OnInit } from '@angular/core';
+import { NbMenuService, NbSidebarService } from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-header',
+  styleUrls: ['./header.component.scss'],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent implements OnInit {
+  @Input() position = 'normal';
+
+  user: any;
+
+  userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
+
+  constructor(
+    private sidebarService: NbSidebarService,
+    private menuService: NbMenuService,
+  ) {}
+
+  ngOnInit() {
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+
+    return false;
+  }
+
+  toggleSettings(): boolean {
+    this.sidebarService.toggle(false, 'settings-sidebar');
+
+    return false;
+  }
+
+  goToHome() {
+    this.menuService.navigateHome();
+  }
+}

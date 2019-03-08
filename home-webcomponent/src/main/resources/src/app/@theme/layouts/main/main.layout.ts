0a6150e970c8b7429d7d8602e0322493561426b0
ak983d <ak983d@att.com>
2019-03-08 13:16:26 -0600

    Initial code check in for ML Workbench UI Project.
    Change-Id: I3e767355b7ed565f9bbacfdf2e63bea94ea21e28
    Signed-off-by: ak983d <ak983d@att.com>

---
+++ home-webcomponent/src/main/resources/src/app/@theme/layouts/main/main.layout.ts
@@ -0,0 +1,84 @@
+/*
+===============LICENSE_START=======================================================
+Acumos Apache-2.0
+===================================================================================
+Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+===================================================================================
+This Acumos software file is distributed by AT&T and Tech Mahindra
+under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+===============LICENSE_END=========================================================
+*/
+
+import { Component, OnDestroy } from '@angular/core';
+import { delay, withLatestFrom, takeWhile } from 'rxjs/operators';
+import {
+  NbMediaBreakpoint,
+  NbMediaBreakpointsService,
+  NbMenuItem,
+  NbMenuService,
+  NbSidebarService,
+  NbThemeService,
+} from '@nebular/theme';
+
+
+// TODO: move layouts into the framework
+@Component({
+  selector: 'ngx-main-layout',
+  styleUrls: ['./main.layout.scss'],
+  templateUrl: './main.layout.html',
+})
+export class MainLayoutComponent implements OnDestroy {
+  subMenu: NbMenuItem[] = [
+    {
+      title: 'PAGE LEVEL MENU',
+    },
+  ];
+  layout: any = {};
+  sidebar: any = {};
+
+  private alive = true;
+
+  currentTheme: string;
+
+  constructor(
+    protected menuService: NbMenuService,
+    protected themeService: NbThemeService,
+    protected bpService: NbMediaBreakpointsService,
+    protected sidebarService: NbSidebarService,
+  ) {
+
+    const isBp = this.bpService.getByName('is');
+    this.menuService
+      .onItemSelect()
+      .pipe(
+        takeWhile(() => this.alive),
+        withLatestFrom(this.themeService.onMediaQueryChange()),
+        delay(20),
+      )
+      .subscribe(
+        ([item, [bpFrom, bpTo]]: [any, [NbMediaBreakpoint, NbMediaBreakpoint]]) => {
+          if (bpTo.width <= isBp.width) {
+            this.sidebarService.collapse('menu-sidebar');
+          }
+        },
+      );
+
+    this.themeService
+      .getJsTheme()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(theme => {
+        this.currentTheme = theme.name;
+      });
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+}
---
+++ home-webcomponent/src/main/resources/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,62 @@
+/*
+===============LICENSE_START=======================================================
+Acumos Apache-2.0
+===================================================================================
+Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+===================================================================================
+This Acumos software file is distributed by AT&T and Tech Mahindra
+under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+===============LICENSE_END=========================================================
+*/
+
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateComponent } from './projects/create/create.component';
+import { CatalogComponent } from './projects/catalog/catalog.component';
+import { ViewComponent } from './projects/view/view.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'projects/create',
+        component: CreateComponent,
+      },
+      {
+        path: 'projects/catalog',
+        component: CatalogComponent,
+      },
+      {
+        path: 'projects/view/:id',
+        component: ViewComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}

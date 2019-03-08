da1e2cded52c83aed00f90c4b1bc279ffd57a758
VaibhavShirsat <vs00485966@techmahindra.com>
2019-03-08 22:58:28 +0530

    Initial Project Commit
    Description : Committed initial code for ML Workbench project-service
    module. Need to further work on Logging and JUnit.
    
    Issue-Id : ACUMOS-2480,ACUMOS-2481,ACUMOS-2482,ACUMOS-2483,ACUMOS-2484
    Change-Id: I6e4c0edc159a6719365fa153a05c1f3d984b00d4
    Signed-off-by: VaibhavShirsat <vs00485966@techmahindra.com>

---
+++ project-service/src/main/java/org/acumos/workbench/projectservice/service/ProjectValidationServiceImpl.java
@@ -0,0 +1,56 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.workbench.projectservice.service;
+
+import org.acumos.workbench.projectservice.vo.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service("ProjectValidationServiceImpl")
+public class ProjectValidationServiceImpl {
+	
+	
+	@Autowired
+	@Qualifier("InputValidationServiceImpl")
+	InputValidationService inputValidationServiceImpl;
+	
+	public void validateInput(String authenticatedUserId, Project project ) { 
+		
+		//1. Validation service to validate the input: only Name, version, and description should be the input values rest should be empty.
+		inputValidationServiceImpl.validateProjectInputJson(project);
+		
+		//2. Check authenticatedUserId should be present
+		inputValidationServiceImpl.isValuePresent("Acumos User Id",authenticatedUserId);
+		
+		//3. Project name should be present
+		inputValidationServiceImpl.isValuePresent("Project Name", project.getProjectId().getName());
+		inputValidationServiceImpl.isValuePresent("Project Version", project.getProjectId().getVersionId().getLabel());
+		
+		// 4. Validate Project Name & Version (for allowed special character)
+		inputValidationServiceImpl.validateProjectName(project.getProjectId().getName());
+		inputValidationServiceImpl.validateVersion(project.getProjectId().getVersionId().getLabel());
+		
+	}
+	
+	
+	
+}

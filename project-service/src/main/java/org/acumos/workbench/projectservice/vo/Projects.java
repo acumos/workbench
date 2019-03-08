f92b1bd2b9e221671cdfd51c488a09a03d00d1da
VaibhavShirsat <vs00485966@techmahindra.com>
2019-03-08 22:58:28 +0530

    Initial Project Commit
    Description : Committed initial code for ML Workbench project-service
    module. Need to further work on Logging and JUnit.
    
    Issue-Id : ACUMOS-2480,ACUMOS-2481,ACUMOS-2482,ACUMOS-2483,ACUMOS-2484
    Change-Id: I6e4c0edc159a6719365fa153a05c1f3d984b00d4
    Signed-off-by: VaibhavShirsat <vs00485966@techmahindra.com>

---
+++ project-service/src/main/java/org/acumos/workbench/projectservice/vo/Projects.java
@@ -0,0 +1,48 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+  * ===================================================================================
+ * Copyright (C) 2019 - 2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.workbench.projectservice.vo;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class Projects implements Serializable {
+
+	private static final long serialVersionUID = -8031420162073734085L;
+	private Project projects;
+
+	/**
+	 * @return the projects
+	 */
+	public Project getProjects() {
+		return projects;
+	}
+
+	/**
+	 * @param projects the projects to set
+	 */
+	public void setProjects(Project projects) {
+		this.projects = projects;
+	}
+	
+}
---
+++ project-service/src/test/java/org/acumos/workbench/projectservice/service/InputValidationServiceImplTest.java
@@ -0,0 +1,5 @@
+package org.acumos.workbench.projectservice.service;
+
+public class InputValidationServiceImplTest {
+
+}

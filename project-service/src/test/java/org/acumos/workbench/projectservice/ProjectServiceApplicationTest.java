d4f243afd9e7f40f6470496685485f89461f3b8f
VaibhavShirsat <vs00485966@techmahindra.com>
2019-03-08 22:58:28 +0530

    Initial Project Commit
    Description : Committed initial code for ML Workbench project-service
    module. Need to further work on Logging and JUnit.
    
    Issue-Id : ACUMOS-2480,ACUMOS-2481,ACUMOS-2482,ACUMOS-2483,ACUMOS-2484
    Change-Id: I6e4c0edc159a6719365fa153a05c1f3d984b00d4
    Signed-off-by: VaibhavShirsat <vs00485966@techmahindra.com>

---
+++ project-service/src/test/java/org/acumos/workbench/projectservice/ProjectServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package org.acumos.workbench.projectservice;
+
+import org.acumos.workbench.projectservice.ProjectServiceApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ProjectServiceApplication.class)
+public class ProjectServiceApplicationTest {
+	@Test
+	public void contextLoads() throws Exception {
+		ProjectServiceApplication.main(new String[0]);
+	}
+}

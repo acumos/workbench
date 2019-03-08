abcc4e65d770064eeae8af40661f6978807b5ba4
ak983d <ak983d@att.com>
2019-03-08 13:16:26 -0600

    Initial code check in for ML Workbench UI Project.
    Issue-ID: ACUMOS-2623
    
    Change-Id: I3e767355b7ed565f9bbacfdf2e63bea94ea21e28
    Signed-off-by: ak983d <ak983d@att.com>

---
+++ home-webcomponent/src/main/resources/src/app/app.module.ts
@@ -0,0 +1,65 @@
+/*
+===============LICENSE_START=======================================================
+Acumos Apache-2.0
+===================================================================================
+Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+===================================================================================
+This Acumos software file is distributed by AT&T and Tech Mahindra
+under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+===============LICENSE_END=========================================================
+*/
+
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './@core/core.module';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ThemeModule } from './@theme/theme.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppConfigService } from './app-config.service';
+import '@material/mwc-icon';
+
+declare module 'lit-html/lib/parts.d' { type bigint = number; }
+
+const configInitializer = (appConfig: AppConfigService) => {
+  return () => {
+    return appConfig.loadAppConfig();
+  };
+};
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+
+    NgbModule.forRoot(),
+    ThemeModule.forRoot(),
+    CoreModule.forRoot(),
+  ],
+  bootstrap: [AppComponent],
+  providers: [
+    AppConfigService,
+    { provide: APP_BASE_HREF, useValue: '/' },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configInitializer,
+      multi: true,
+      deps: [AppConfigService],
+    },
+  ],
+})
+export class AppModule {}

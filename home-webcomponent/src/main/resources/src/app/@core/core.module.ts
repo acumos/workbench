6c778102e388ce68f7205d71888a5f40a0d8e053
ak983d <ak983d@att.com>
2019-03-08 13:16:26 -0600

    Initial code check in for ML Workbench UI Project.
    Change-Id: I3e767355b7ed565f9bbacfdf2e63bea94ea21e28
    Signed-off-by: ak983d <ak983d@att.com>

---
+++ home-webcomponent/src/main/resources/src/app/@core/core.module.ts
@@ -0,0 +1,48 @@
+/*
+===============LICENSE_START=======================================================
+Acumos Apache-2.0
+===================================================================================
+Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+===================================================================================
+This Acumos software file is distributed by AT&T and Tech Mahindra
+under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+===============LICENSE_END=========================================================
+*/
+
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { throwIfAlreadyLoaded } from './module-import-guard';
+import { DataModule } from './utils/data.module';
+
+export const CORE_PROVIDERS = [
+  ...DataModule.forRoot().providers,
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  exports: [],
+  declarations: [],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+
+  static forRoot(): ModuleWithProviders {
+    return <ModuleWithProviders>{
+      ngModule: CoreModule,
+      providers: [
+        ...CORE_PROVIDERS,
+      ],
+    };
+  }
+}

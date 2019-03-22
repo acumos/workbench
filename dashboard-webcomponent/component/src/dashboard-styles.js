379521359f662ffe4d22b6ee72c8a55028145a13
ak983d <ak983d@att.com>
2019-03-22 17:44:49 -0500

    UI integration with Node express server
    Change-Id: I15363822a5a6d2ee318443b14db9180c348aceb8
    Issue-ID: Acumos-2623
    Signed-off-by: ak983d <ak983d@att.com>

---
+++ dashboard-webcomponent/component/src/dashboard-styles.js
@@ -0,0 +1,42 @@
+/*
+===============LICENSE_START=======================================================
+Acumos Apache-2.0
+===================================================================================
+Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+===================================================================================
+This Acumos software file is distributed by AT&T and Tech Mahindra
+under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+===============LICENSE_END=========================================================
+*/
+
+import {css} from 'lit-element/lit-element.js';
+export const style = css`
+  #divid {
+    border: thin solid darkblue;
+    padding: 25px;
+    border-radius: 5px;
+  }
+
+  .btn-primary {
+    background-color: #591887;
+    border-color: transparent;
+  }
+
+  .btn-outline-primary {
+    color: #591887;
+    border-color: #591887;
+  }
+
+  label {
+    color: #4b4b4b;
+  }
+`;

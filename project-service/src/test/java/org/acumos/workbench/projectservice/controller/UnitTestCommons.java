eb1c98643c69972f9490113dabb3b99733029787
Ramanaiah Pirla <RP00490596@techmahindra.com>
2019-03-29 17:43:17 +0530

    Added ArchiveProject Impl and JUnit tests
    Issue-Id : ACUMOS-2480,ACUMOS-2481,ACUMOS-2482,ACUMOS-2483,ACUMOS-2484
    Description : Committed initial code for ML Workbench project-service
    module Junit test cases.
    
    Change-Id: I9ed316a5bc71d41ff31af3a0db06f88107100595
    Signed-off-by: Ramanaiah Pirla <RP00490596@techmahindra.com>

---
+++ project-service/src/test/java/org/acumos/workbench/projectservice/controller/UnitTestCommons.java
@@ -0,0 +1,94 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.workbench.projectservice.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.acumos.workbench.projectservice.util.ArtifactStatus;
+import org.acumos.workbench.projectservice.util.IdentifierType;
+import org.acumos.workbench.projectservice.vo.ArtifactState;
+import org.acumos.workbench.projectservice.vo.Identifier;
+import org.acumos.workbench.projectservice.vo.Models;
+import org.acumos.workbench.projectservice.vo.Notebooks;
+import org.acumos.workbench.projectservice.vo.Organization;
+import org.acumos.workbench.projectservice.vo.Pipelines;
+import org.acumos.workbench.projectservice.vo.Project;
+import org.acumos.workbench.projectservice.vo.Projects;
+import org.acumos.workbench.projectservice.vo.Role;
+import org.acumos.workbench.projectservice.vo.User;
+import org.acumos.workbench.projectservice.vo.Version;
+
+public abstract class UnitTestCommons {
+
+	public UnitTestCommons() {
+		super();
+	}
+
+	protected Project buildProject() {
+		Project project = new Project();
+		ArtifactState artifactState = new ArtifactState();
+		artifactState.setStatus(ArtifactStatus.ACTIVE);
+		project.setArtifactStatus(artifactState);
+		project.setDescription("Project Description");
+		Models models = new Models();
+		project.setModels(models);
+		Notebooks notebooks = new Notebooks();
+		project.setNotebooks(notebooks);
+		User owner = new User();
+		owner.setAuthenticatedUserId("123");
+		owner.setModels(models);
+		owner.setNotebooks(notebooks);
+		Organization organization = new Organization();
+		owner.setOrganization(organization);
+		Pipelines pipelines = new Pipelines();
+		owner.setPipelines(pipelines);
+		Role roles = new Role();
+		owner.setRoles(roles);
+		Identifier userId = new Identifier();
+		userId.setUuid("123");
+		userId.setName("TechMDev");
+		userId.setServiceUrl("https://acumos.org");
+		userId.setIdentifierType(IdentifierType.PIPELINE);
+		owner.setUserId(userId);
+		List<Project> projectList = new ArrayList<Project>();
+		projectList.add(project);
+		Projects projects = new Projects();
+		projects.setProjects(projectList);
+		owner.setProjects(projects);
+		project.setOwner(owner);
+		Identifier identifier = new Identifier();
+		identifier.setIdentifierType(IdentifierType.PIPELINE);
+		identifier.setName("IdentifierName");
+		identifier.setServiceUrl("https://acumos.org");
+		identifier.setRepositoryUrl("https://acumos.org");
+		identifier.setUuid("123");
+		Version version = new Version();
+		version.setComment("comment");
+		version.setLabel("Label");
+		version.setTimeStamp("");
+		version.setUser("123");
+		identifier.setVersionId(version);
+		project.setProjectId(identifier);
+		return project;
+	}
+
+}

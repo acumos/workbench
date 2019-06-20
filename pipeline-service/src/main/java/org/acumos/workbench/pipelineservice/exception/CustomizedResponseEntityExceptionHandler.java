e0850d9e23866424515ca85cb06e546eff34cf7e
Ramanaiah Pirla <RP00490596@techmahindra.com>
2019-06-20 18:04:40 +0530

    Modified Pipeline Code as per new Req
    Issue-Id : ACUMOS-2505
    Description : Code added for create pipleine in NiFi and
    modified the code as per new requirement
    
    Change-Id: I0342867043494b201714e38361627765dad62f2a
    Signed-off-by: Ramanaiah Pirla <RP00490596@techmahindra.com>

--- pipeline-service/src/main/java/org/acumos/workbench/pipelineservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ pipeline-service/src/main/java/org/acumos/workbench/pipelineservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -26,11 +26,15 @@ import org.acumos.workbench.common.exception.ArchivedException;
 import org.acumos.workbench.common.exception.BadRequestException;
 import org.acumos.workbench.common.exception.EntityNotFoundException;
 import org.acumos.workbench.common.exception.ForbiddenException;
+import org.acumos.workbench.common.logging.LoggingConstants;
 import org.acumos.workbench.common.util.ServiceStatus;
 import org.acumos.workbench.common.vo.Pipeline;
 import org.acumos.workbench.common.vo.ServiceState;
+import org.acumos.workbench.pipelineservice.service.PipeLineCacheService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -45,6 +49,8 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+	@Autowired
+	private PipeLineCacheService pipelineCacheService;
 	
 	/**
 	 * The method to handle BadRequestException and return the appropriate response to UI. 
@@ -58,9 +64,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	 */
 	@ExceptionHandler(BadRequestException.class)
 	public final ResponseEntity<?> handleBadRequestException(BadRequestException ex, WebRequest request) {
+		removeRequestFromCache(request);
 		Pipeline pipeline = getPipeline(ex);
 		return new ResponseEntity<Pipeline>(pipeline, HttpStatus.BAD_REQUEST);
 	}
+
 	
 	/**
 	 * The method to handle ForbiddenException and return the appropriate response to UI.
@@ -74,6 +82,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	 */
 	@ExceptionHandler(ForbiddenException.class)
 	public final ResponseEntity<?> handleBadRequestException(ForbiddenException ex, WebRequest request) {
+		removeRequestFromCache(request);
 		Pipeline pipeline = getPipeline(ex);
 		return new ResponseEntity<Pipeline>(pipeline, HttpStatus.FORBIDDEN);
 	}
@@ -90,6 +99,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	 */
 	@ExceptionHandler(ArchivedException.class)
 	public final ResponseEntity<?> handleArchivedException(ForbiddenException ex, WebRequest request) {
+		removeRequestFromCache(request);
 		Pipeline pipeline = getPipeline(ex);
 		return new ResponseEntity<Pipeline>(pipeline, HttpStatus.LOCKED);
 	}
@@ -105,6 +115,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	 */
 	@ExceptionHandler(EntityNotFoundException.class)
 	public final ResponseEntity<?> handleArtifactNotFoundException(EntityNotFoundException ex, WebRequest request) {
+		removeRequestFromCache(request);
 		Pipeline pipeline = getPipeline(ex);
 		return new ResponseEntity<Pipeline>(pipeline, HttpStatus.NOT_FOUND);
 	}
@@ -121,6 +132,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	@ExceptionHandler(RestClientResponseException.class)
 	public final ResponseEntity<?> handleRestClientResponseException(RestClientResponseException ex, WebRequest request) { 
 		logger.debug("handleRestClientResponseException() Begin",ex.getResponseBodyAsString());
+		removeRequestFromCache(request);
 		Pipeline pipeline = new Pipeline();
 		ServiceState serviceState = new ServiceState();
 		serviceState.setStatus(ServiceStatus.ERROR);
@@ -128,6 +140,23 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		pipeline.setServiceStatus(serviceState);
 		logger.debug("handleRestClientResponseException() End");
 		return new ResponseEntity<Pipeline>(pipeline, HttpStatus.METHOD_FAILURE);
+	}
+	
+	/**
+	 * The method to handle DuplicateRequestException and return the appropriate response to UI. 
+	 * 
+	 * @param ex
+	 * 		the exception thrown 
+	 * @param request
+	 * 		the Web request. 
+	 * @return ResponseEntitiy<Pipeline> 
+	 * 		returns Pipeline with ServiceStatus indicating error 
+	 */
+	@ExceptionHandler(DuplicateRequestException.class)
+	public final ResponseEntity<?> handleDuplicateRequestException(DuplicateRequestException ex, WebRequest request) {
+		removeRequestFromCache(request);
+		Pipeline pipeline = getPipeline(ex);
+		return new ResponseEntity<Pipeline>(pipeline, HttpStatus.BAD_REQUEST);
 	}
 	
 	private Pipeline getPipeline(RuntimeException ex) {
@@ -140,5 +169,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		logger.debug("getPipeline() End");
 		return pipeline;
 	}
-	 
+
+	private void removeRequestFromCache(WebRequest request) {
+		String requestId = request.getHeader(LoggingConstants.Headers.REQUEST_ID);
+		if(null == requestId ) {
+			requestId = MDC.get(LoggingConstants.MDCs.REQUEST_ID);
+		}
+		pipelineCacheService.removeRequest(requestId);
+	}
+	
 }

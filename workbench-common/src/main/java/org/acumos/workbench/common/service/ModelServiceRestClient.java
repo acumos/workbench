fd3cc4937f62f8cf3657945bf21e7f5ab25f8613
RP00490596 <RP00490596@TechMahindra.com>
2019-09-27 16:18:16 +0530

    PredService RestTemplate code added in wb-commons
    Change-Id: Iaa0bbfaa32d1d1104ee1c89a06fa166b73c81c9b
    Signed-off-by: RP00490596 <RP00490596@TechMahindra.com>

---
+++ workbench-common/src/main/java/org/acumos/workbench/common/service/ModelServiceRestClient.java
@@ -0,0 +1,45 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+
+package org.acumos.workbench.common.service;
+
+import java.util.List;
+
+import org.acumos.workbench.common.vo.Model;
+import org.springframework.http.ResponseEntity;
+
+public interface ModelServiceRestClient {
+	
+	/**
+	 * Get Models for a particular projectId
+	 * 
+	 * @param authenticatedUserId
+	 * 		Acumos User login Id
+	 * @param projectId
+	 * 		Project Id 
+	 * @param authToken
+	 * 		JWT Auth Token 
+	 * @return ResponseEntity<List<Model>>
+	 * 		returns List of Models
+	 */
+	ResponseEntity<List<Model>> getModels(String authenticatedUserId,String projectId, String authToken);
+
+}

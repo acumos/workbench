83b23e6f0322568e7909f1e06b5bc30f87c8f74d
Shaik Ghannubee <SG00587246@TechMahindra.com>
2020-05-08 18:59:41 +0530

    home component channges for datasource
    Issue-ID: Acumos-4076
    Description: Datasource UI changes
    
    Change-Id: I80bdaf3db0afdcadef7cfc4934c667107053ab40
    Signed-off-by: Shaik Ghannubee <SG00587246@TechMahindra.com>

---
+++ home-webcomponent/component/src/app/pages/datasource/view/view.component.ts
@@ -0,0 +1,64 @@
+/*
+===============LICENSE_START=======================================================
+Acumos Apache-2.0
+===================================================================================
+Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+===================================================================================
+This Acumos software file is distributed by AT&T and Tech Mahindra
+under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+===============LICENSE_END=========================================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ScriptService } from '../../../@core/utils/script.service';
+import { BreadcrumbsService } from '../../../@core/utils/breadcrumbs.service';
+import { BaseComponent } from '../../base/base.component';
+
+@Component({
+  templateUrl: './view.component.html',
+})
+export class DatasourceViewComponent extends BaseComponent implements OnInit {
+
+  public id: string;
+  public name: string;
+  public router: Router;
+  script: ScriptService;
+  public datasourceComponentURL: string;
+  public userName: any;
+  public authToken: any;
+  public sessionError: any;
+  public alertOpen: any;
+  public breadCrumbs: any[] = [
+    { name: 'Home', href: '' },
+    { name: 'Design Studio', href: '' },
+    { name: 'ML Workbench', sref: '/pages/dashboard' },
+    { name: 'Datasource', sref: '/pages/datasource/catalog' }];
+
+  constructor(private route: ActivatedRoute, router: Router, script: ScriptService, breadcrumbsService: BreadcrumbsService) {
+    super(router, script, breadcrumbsService);
+  }
+
+  OnViewDatasourceEvent(e) {
+    if (e.detail[0].data === 'catalog-datasource') {
+      this.router.navigateByUrl('/pages/datasource/catalog');
+    }
+  }
+
+  ngOnInit() {
+    this.loadHtml = false;
+    this.showSpinner = true;
+    this.alertOpen = false;
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.name = this.route.snapshot.paramMap.get('name');
+    this.breadCrumbs.push({ name: this.name });
+    this.loadComponent('datasourceComponent', 'datasource-webcomponent-element', this.breadCrumbs);
+  }
+}
